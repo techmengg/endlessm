9801bf195877e273ec60b91adac20b35808fff72
techmeng aing <aingtechmeng@gmail.com>
2024-05-19 23:50:55 -0400

    centered nav bar mobile ul and hide scroll wheel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,26 @@
       }, 1000)
   }
 
+  function copyText() {
+    // Get the div element
+    var divElement = document.getElementById("copytextid");
   
+    // Create a range object
+    var range = document.createRange();
+  
+    // Select the contents of the div element
+    range.selectNode(divElement);
+  
+    // Add the range to the user's selection
+    window.getSelection().addRange(range);
+  
+    // Copy the selected text to the clipboard
+    document.execCommand("copy");
+  
+    // Give a visual feedback to the user that the text has been copied
+    alert("Text has been copied!");
+  }
+
 
   /**
    * Easy selector helper function
